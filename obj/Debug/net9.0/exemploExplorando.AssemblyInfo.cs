300cdf5cc00b3a51975f9a10fbd720929583b146
Larissa Valli <larissavalli4@gmail.com>
2025-08-05 18:36:38 -0300

    .

--- obj/Debug/net9.0/exemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net9.0/exemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43123a7ba4997e2451198ab921492d9f1136d84d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e4faf07388336ff983a078557f6d21d9579a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
