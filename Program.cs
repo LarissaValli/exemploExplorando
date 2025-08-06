e0a91298a208a20bcab43915bdb2bdafd574be23
Larissa Valli <larissavalli4@gmail.com>
2025-08-06 11:34:21 -0300

    construtores

--- Program.cs
+++ Program.cs
@@ -1,7 +1,50 @@
 ﻿using exemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Luna";
-p1.Sobrenome = "Valli";
-p1.Idade = 23;
-p1.Apresentar();
+Pessoa p1 = new Pessoa(nome:"Luna", sobrenome: "Valli");
+Pessoa p2 = new Pessoa(nome: "Alexandre", sobrenome: "Contardi");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Luna";
+// p1.Sobrenome = "Valli";
+// p1.Idade = 23;
+// p1.Apresentar();
